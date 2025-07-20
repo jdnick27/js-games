08fe32c8b6d088a30782b98e8c31fe70e14961d0
Jayden Nicholson <jdnick27@gmail.com>
2025-07-19 21:54:32 -0500

    pr fix

--- cleanupBranches.sh
+++ cleanupBranches.sh
@@ -2,4 +2,9 @@
 # Delete local branches that have been removed from the remote
 # Saves the list of deleted branches to deleted_branches.txt
 
+Add this as a script in the project. 
+
 git branch -vv | awk '/: gone]/{print $1}' | tee deleted_branches.txt | xargs -r git branch -D
+
+
+Also, organize the game files into better structure.
